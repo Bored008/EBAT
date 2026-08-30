536859c78be8241220e7ff89881a5232ae6bcbf3
Bored008 <dahiyahimanshu80@gmail.com>
2026-08-30 17:39:10 +0530

    style: reduce spacing between Services header and Masonry grid

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,8 +11,10 @@ export default function ServicesPage() {
       <main className="w-full flex-1 flex flex-col items-center pt-[60px] md:pt-[120px] px-6 md:px-[90px]">
         {/* Main Container */}
         <div className="flex flex-col gap-[80px] md:gap-[120px] w-full max-w-[1260px] mx-auto pb-[86px]">
-          <ServicesHeader />
-          <MasonryGrid />
+          <div className="flex flex-col gap-[40px] md:gap-[60px] w-full">
+            <ServicesHeader />
+            <MasonryGrid />
+          </div>
           <FixedWingSupport />
           <CertificationSection />
         </div>
